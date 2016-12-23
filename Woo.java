00fdf96dfacf26c2e4c7825e29635f715e556404
Rihui Zheng <Rihui Zheng>
2016-12-23 12:22:37 -0800

    Beginning Message

---
+++ Woo.java
@@ -0,0 +1,5 @@
+public class Woo {
+    public static void main(String[] arr) {
+	System.out.println("Welcome to the World of Pokemon!\nThis world is inhabited by mysetrious creatures known as Pokemon.\nI am professor Topher.");
+    }
+}
