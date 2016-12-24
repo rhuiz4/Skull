1fdc32cc179d75f9068c09bf43996f6d84642957
Rihui Zheng <Rihui Zheng>
2016-12-24 12:07:24 -0800

    Hi

--- Woo.java
+++ Woo.java
@@ -1,5 +1,22 @@
+//Team Skull: Rihui Zheng, Jake Zaia, Tim Wang
+//APCS Pd5
+//Final Project: Gotta Catch 'Em All
+
 public class Woo {
+    private int HP;
+    private int Atk;
+    private int Def;
+    private int SpA;
+    private int SpD;
+    private int Sp;
+
+    public Woo {
+	
+    }
+    
     public static void main(String[] arr) {
-	System.out.println("Welcome to the World of Pokemon!\nThis world is inhabited by mysetrious creatures known as Pokemon.\nI am professor Topher.");
+	System.out.println("Welcome to the World of Pokemon!\n
+                            This world is inhabited by mysetrious creatures known as Pokemon.\n
+                            I am professor Topher.");
     }
 }
