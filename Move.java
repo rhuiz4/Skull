816db6c9629869ad27dbb6fbb519c830593416f9
Jake Zaia <jzaia@stuy.edu>
2017-01-12 11:48:04 -0500

    Made moves

---
+++ Move.java
@@ -0,0 +1,10 @@
+public class Move{
+    private String _name, _type;
+    private int _basePow;
+    private float _acc;
+    private bool _isPhysical, _makesContact;
+    
+    public int void use(Pokemon foe);
+
+
+}
