e5df24f84a00300d48b34e97ddbae9ebbf6cc716
Rihui Zheng <rzheng4@stuy.edu>
2017-01-12 15:43:08 -0500

    Pikaaaa

--- Woo.java
+++ Woo.java
@@ -3,20 +3,24 @@
 //Final Project: Gotta Catch 'Em All
 
 public class Woo {
-    private int HP;
-    private int Atk;
-    private int Def;
-    private int SpA;
-    private int SpD;
-    private int Sp;
-
-    public Woo {
-	
-    }
     
     public static void main(String[] arr) {
-	System.out.println("Welcome to the World of Pokemon!\n
-                            This world is inhabited by mysetrious creatures known as Pokemon.\n
-                            I am professor Topher.");
+	//Openning Message
+	/*	System.out.println("Hello! Sorry to keep you waiting!\n
+                            Welcome to the World of Pokémon!\n
+                            My name is Oak. People affectionately refer to me as the Pokémon Professor.\n
+                            This world is inhabited far and wide by creatures called Pokémon!\n
+                            For some people, Pokémon are pets. Other use them for battling.\n
+                            As for myself… I study Pokémon as a profession. But first, tell me a little about yourself.\n
+                            ");*/
+	Pokemon opponent = new Pikachu();
+	Pokemon player = new Pikachu();
+	System.out.println("A wild Pikachu appeared!\nGo Pikachu");
+	System.out.println("\n\nOpponent's Pikachu\nHP: " + "opponent.HP" + "%");
+
+	System.out.println("\n\nPlayer's Pikachu\nHP: " + "player.HP" + "%");
+	System.out.println("\nWhat will Pikachu do?\n" + player._move1 + "\t" + player._move2 + "\n" + player._move3 + "/t" + player._move4);
+	
+
     }
 }
