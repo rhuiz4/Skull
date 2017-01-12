ec7a81d06d7bebfc6b077bc455d5667be8172c6e
Annie Gu <Annie Gu>
2017-01-12 18:17:44 -0500

    Pikachu

---
+++ Pikachu.java
@@ -0,0 +1,15 @@
+public class Pikachu extends Pokemon{
+    
+    public Pikachu(){
+	_hp = (int)(Math.random() *48 + 95);
+	_spdef = (int)(Math.random() *64 + 49);
+	_spatk = (int)(Math.random() *64 + 49);
+	_speed = (int)(Math.random() *72 + 85);
+	_atk = (int)(Math.random() *64 + 54);
+	_def = (int)(Math.random() *62 + 40);
+	_curr = _hp;
+	_type1 = "Electric";
+    }
+
+}
+	
