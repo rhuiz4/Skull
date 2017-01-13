3da38e6aa5ea28444131a4dbc6b1c0e9bc3b112d
timbarcavilla <zwang6@stuy.edu>
2017-01-12 21:53:49 -0500

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -9,14 +9,10 @@ public abstract class Pokemon{
     public Move _move3;
     public Move _move4;
 
-<<<<<<< HEAD
+
     public void takeDamage(int d){
 	_currhp -= d;
     }
-=======
-    public void takeDamage(int d){ _currhp -= d};
->>>>>>> ec4def4fdb20c5a0ef778d4a8c4a2763e95c0676
-
 
     //Move using methods
     public void move1(Pokemon p){
