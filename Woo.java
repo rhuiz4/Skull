c9ead217dbe4e632395f9fe9d30d6673a08c93db
Rihui Zheng <Rihui Zheng>
2017-01-12 18:33:05 -0800

    added comments on woo

--- Woo.java
+++ Woo.java
@@ -6,11 +6,11 @@ import csi.Keyboard;
 public class Woo {
     
     public static void main(String[] arr) {
-	
+	double oppHP = 0;
+	double playHP = 0;
 	Pokemon opponent = new Pikachu();
 	Pokemon player = new Pikachu();
 	System.out.println("A wild Pikachu appeared!\nGo Pikachu");
-
 	int playerMove = 0;
 	int opponentMove = 0;
 	boolean validMove = false;
@@ -19,11 +19,16 @@ public class Woo {
 	boolean battleOver = false;
 	
 	while (!battleOver) {
-	    System.out.println("\n\nOpponent's Pikachu\nHP: " + "opponent.HP" + "%");
-	    System.out.println("\n\nPlayer's Pikachu\nHP: " + "player.HP" + "%");
+	    //displays HP
+	    oppHP = opponent.getCurrHP() / opponent.getMaxHP();
+	    playHP = player.getCurrHP() / player.getMaxHP();
+	    System.out.println("\n\nOpponent's Pikachu\nHP: " + oppHP + "%");
+	    System.out.println("\n\nPlayer's Pikachu\nHP: " + playHP + "%");
 	    
+	    //displays moves
 	    System.out.println("\nWhat will Pikachu do?\nPlease enter the number corresponding to the move you want to use:\n1) " + player._move1 + "\t2) " + player._move2 + "\n3) " + player._move3 + "/t4) " + player._move4);
 
+	    //keeps on asking the user to put in integer until they input a valid input
 	    while (!validMove) {
 		playerMove = Keyboard.readInt();
 		if (playerMove >= 1 && playerMove <= 4) {
@@ -37,7 +42,7 @@ public class Woo {
 	    
 
 	    //if enemy is faster
-	    if (opponent._speed > player._speed) {
+	    if (opponent.getSpeed() > player.getSpeed()) {
 		opponentMove = (int)(Math.random * 5);		
 		if (opponentMove == 1){
 		    System.out.println("The foe's Pikachu used " + opponent._move1 + "!");
@@ -57,7 +62,7 @@ k		    opponent.move4(player);
 		}
 
 		//if player's Pokemon is still alive
-		if (player._HP > 0){
+		if (player.getCurrHP() > 0){
 		    if (playerMove == 1){
 			System.out.println("Pikachu used " + player._move1 + "!");
 			player.move1(opponent);
@@ -79,8 +84,8 @@ k		    opponent.move4(player);
 		    playerFainted = true;
 	    }
 
-	    //if player is faster
-	    if (opponent._speed < player._speed) {
+	    //if player is faster or tied
+	    if (opponent.getSpeed() <= player._getSpeed()) {
 		if (playerMove == 1){
 			System.out.println("Pikachu used " + player._move1 + "!");
 			player.move1(opponent);
@@ -98,7 +103,7 @@ k		    opponent.move4(player);
 			player.move4(opponent);
 		    }
 		//if enemy is still alive
-		if (opponent._HP > 0) {
+		if (opponent.getCurrHP() > 0) {
 		    opponentMove = (int)(Math.random * 5);		
 		    if (opponentMove == 1){
 			System.out.println("The foe's Pikachu used " + opponent._move1 + "!");
