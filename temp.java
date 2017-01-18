b729a3ce3cfb95fcbb0eb1111465b8b0b456fc19
Jake Zaia <jzaia@stuy.edu>
2017-01-18 11:41:54 -0500

    finished zipImage

--- temp.java
+++ temp.java
@@ -3,20 +3,27 @@ public class temp{
 	String s1 = p1.toString();
 	String s2 = p2.toString();
 	String retStr = "";
-	int stop = s1.length() + s2.length();
-	while (s1.length() > 0 && s2.length() > 0){
-	    retStr += s1.substring(0, s1.indexOf("\n"));
-	    retStr += s2.substring(0, s2.indexOf("\n"));
-	    s1 = s1.substring(s1.indexOf("\n" + 1));
-	    s1 = s1.substring(s2.indexOf("\n" + 1));
+	while (s1.length() > 0 || s2.length() > 0){
+	    if (s1.indexOf("\n") == -1){
+		retStr += s1 + "     ";
+		s1 = "";}
+	    else{
+		retStr += s1.substring(0, s1.indexOf("\n")) + "     ";
+		s1 = s1.substring(s1.indexOf("\n") + 1);}
+	    if (s2.indexOf("\n") == -1){
+		retStr += s2 + "\n";
+		s2 = "";}
+	    else{
+		retStr += s2.substring(0, s2.indexOf("\n")) + "\n";
+		s2 = s2.substring(s2.indexOf("\n") + 1);}
 	}
 	return retStr;
     }
 
 
     public static void main(String [] args){
-	Pokemon p1 = new Pikachu();
-	Pokemon p2 = new Charizard();
-	System.out.println(zipImage(p1, p2));
+	Pokemon p1 = new Charizard();
+	Pokemon p2 = new Pikachu();
+	System.out.println(zipImage(p1,p2));
     }
 }
